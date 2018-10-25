a529a48e42c5a25dfd213edf0f3b70e9eef88681
pl4g4 <ronald@ronaldiraheta.com>
2018-10-24 22:07:55 -0500

    updating reverseiplookup

--- hackertarget.go
+++ hackertarget.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	ipAddress := flag.String("ip", "127.0.0.1", "The ip address. Could be a IP or Domain")
-	hackerTargetOption := flag.String("option", "whois", "MTR Traceroute:mtr \nPing:nping DNS \nLookup:dnslookup \nReverse DNS Lookup:reversedns \nWhois Lookup:whois \nGeoIP:geoip \nReverse IP:reverseip \nHTTP Headers: httpheaders \nPage Link:pagelinks")
+	hackerTargetOption := flag.String("option", "whois", "MTR Traceroute:mtr \nPing:nping DNS \nLookup:dnslookup \nReverse DNS Lookup:reversedns \nWhois Lookup:whois \nGeoIP:geoip \nReverse IP:reverseiplookup \nHTTP Headers: httpheaders \nPage Link:pagelinks")
 	flag.Parse()
 	response := hackertargetapi.GetHackerTargetResponse(*hackerTargetOption, *ipAddress)
 	fmt.Println(response)
